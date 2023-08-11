072d9299f4c5a170cb160805e96471fdee03b6c7
Valentyn Chubukin <val@valch.name>
2023-08-12 00:45:29 +0200

    another-branch commit #2

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world from another-branch!"
+    return "Hello world from another-branch 2!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
