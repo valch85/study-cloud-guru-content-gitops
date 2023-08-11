5ab8bfae12af6fa7366bc92325569e00e6b57c93
Valentyn Chubukin <val@valch.name>
2023-08-12 00:36:00 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Val after adding pwd to github actions!"
+    return "Hello Val after adding pwd to github actions new try!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
